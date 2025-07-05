d51ccaebb9687dd2ad4d869b48d73292d6a61683
Sanjay-Mudela <sanjaymudela@gmail.com>
2025-07-06 01:02:47 +0530

    using proper convention to make our component commit

--- 01_ViteReact/src/App.jsx
+++ 01_ViteReact/src/App.jsx
@@ -1,9 +1,15 @@
+import Chai from './Chai.jsx'
 
 function App() {
 
   return (
+    /*
+      // Here <> </> called 'fragment' act as a <div> </div> element. So we can easily store multiple element inside it. If we don't use it then we can only story a single element in .jsx file. Vite force us to use .jsx file extension to work properly.
+    */
+   
     <>
       <h1> React with Vite </h1>
+      <Chai/>
     </>
   )
 }
--- 01_ViteReact/src/chai.jsx
+++ 01_ViteReact/src/chai.jsx
@@ -1,3 +1,7 @@
+/*
+  // Components/Functions name first letter should be in capital, so it can work properly. If you use lower first letter then it will not work. Your component file name can be in lower case but for convention use Capitalize name.
+*/
+
 function Chai(){
   return (
     <h3> Chai with Sanjay </h3>
